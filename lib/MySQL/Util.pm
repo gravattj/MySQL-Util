5fc768d46f7708b45d8b1a776b7dd7d52da41fc3
gravattj <john@gravatt.org>
2015-12-02 13:02:41 -0600

    .

--- lib/MySQL/Util.pm
+++ lib/MySQL/Util.pm
@@ -17,11 +17,11 @@ MySQL::Util - Utility functions for working with MySQL.
 
 =head1 VERSION
 
-Version 0.25
+Version 0.27
 
 =cut
 
-our $VERSION = '0.26';
+our $VERSION = '0.27';
 
 =head1 SYNOPSIS
 
